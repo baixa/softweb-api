86789d377783dad1de141c36409875ec67cdb7db
Maksimchuk Ivan <baixa2003@gmail.com>
2023-02-15 22:06:05 +0300

    #1 - Add Store module

---
+++ store-api/src/test/java/com/softweb/api/store/StoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softweb.api.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
