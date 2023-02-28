2cfcf55f2b02c55c003f3e16fc52b96fd9f2549a
Maksimchuk Ivan <baixa2003@gmail.com>
2023-02-28 17:07:10 +0300

    #15 - Add config module

--- store-api/src/main/java/com/softweb/api/store/controllers/HomeController.java
+++ store-api/src/main/java/com/softweb/api/store/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.softweb.api.store.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class HomeController {
+
+//    @Value("${test.phrase}")
+    private String phrase;
+
     @GetMapping
     public String main() {
         return "Welcome!";
+    }
+
+    @GetMapping
+    public String verificateConfigServer() {
+        return phrase;
     }
 }
